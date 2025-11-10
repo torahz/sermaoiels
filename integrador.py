fce555865b150e0fbe6e788c42bd365a15d7a0a7
torahz <rafaeltorahz@gmail.com>
2025-11-10 12:15:41 -0300

    Correções no integrador e envio de sermão

--- integrador.py
+++ integrador.py
@@ -1,4 +1,3 @@
-# integrador.py
 from datetime import datetime
 from bs4 import BeautifulSoup
 import unicodedata
@@ -18,58 +17,73 @@ MESES_PT = [
 
 # calendário (mantido igual)
 calendario = {
-    "05/01/2025": ("1º Domingo após o Natal", "natal"),
-    "12/01/2025": ("1º Domingo após a Epifania", "epifania"),
-    "19/01/2025": ("2º Domingo após a Epifania", "epifania"),
-    "26/01/2025": ("3º Domingo após a Epifania", "epifania"),
-    "02/02/2025": ("4º Domingo após a Epifania", "epifania"),
-    "09/02/2025": ("5º Domingo após a Epifania", "epifania"),
-    "16/02/2025": ("6º Domingo após a Epifania", "epifania"),
-    "23/02/2025": ("7º Domingo após a Epifania", "epifania"),
-    "02/03/2025": ("Domingo da Transfiguração do Senhor", "epifania"),
-    "09/03/2025": ("1º Domingo da Quaresma", "quaresma"),
-    "16/03/2025": ("2º Domingo da Quaresma", "quaresma"),
-    "23/03/2025": ("3º Domingo da Quaresma", "quaresma"),
-    "30/03/2025": ("4º Domingo da Quaresma", "quaresma"),
-    "06/04/2025": ("5º Domingo da Quaresma", "quaresma"),
-    "13/04/2025": ("Domingo de Ramos", "quaresma"),
-    "20/04/2025": ("Domingo da Ressurreição", "pascoa"),
-    "27/04/2025": ("2º Domingo de Páscoa", "pascoa"),
-    "04/05/2025": ("3º Domingo de Páscoa", "pascoa"),
-    "11/05/2025": ("4º Domingo de Páscoa", "pascoa"),
-    "18/05/2025": ("5º Domingo de Páscoa", "pascoa"),
-    "25/05/2025": ("6º Domingo de Páscoa", "pascoa"),
-    "01/06/2025": ("7º Domingo de Páscoa", "pascoa"),
-    "08/06/2025": ("Domingo de Pentecostes", "pentecostes"),
-    "15/06/2025": ("1º Domingo após Pentecostes", "tempo-comum"),
-    "22/06/2025": ("2º Domingo após Pentecostes", "tempo-comum"),
-    "29/06/2025": ("3º Domingo após Pentecostes", "tempo-comum"),
-    "06/07/2025": ("4º Domingo após Pentecostes", "tempo-comum"),
-    "13/07/2025": ("5º Domingo após Pentecostes", "tempo-comum"),
-    "20/07/2025": ("6º Domingo após Pentecostes", "tempo-comum"),
-    "27/07/2025": ("7º Domingo após Pentecostes", "tempo-comum"),
-    "03/08/2025": ("8º Domingo após Pentecostes", "tempo-comum"),
-    "10/08/2025": ("9º Domingo após Pentecostes", "tempo-comum"),
-    "17/08/2025": ("10º Domingo após Pentecostes", "tempo-comum"),
-    "24/08/2025": ("11º Domingo após Pentecostes", "tempo-comum"),
-    "31/08/2025": ("12º Domingo após Pentecostes", "tempo-comum"),
-    "07/09/2025": ("1º Domingo da Criação", "criacao"),
-    "14/09/2025": ("2º Domingo da Criação", "criacao"),
-    "21/09/2025": ("3º Domingo da Criação", "criacao"),
-    "28/09/2025": ("4º Domingo da Criação", "criacao"),
-    "05/10/2025": ("13º Domingo após Pentecostes", "tempo-comum"),
-    "12/10/2025": ("14º Domingo após Pentecostes", "tempo-comum"),
-    "19/10/2025": ("15º Domingo após Pentecostes", "tempo-comum"),
-    "26/10/2025": ("16º Domingo após Pentecostes (Reforma Protestante)", "tempo-comum"),
-    "02/11/2025": ("17º Domingo após Pentecostes", "tempo-comum"),
-    "09/11/2025": ("18º Domingo após Pentecostes", "tempo-comum"),
-    "16/11/2025": ("19º Domingo após Pentecostes", "tempo-comum"),
-    "23/11/2025": ("Domingo de Cristo Rei", "tempo-comum"),
-    "30/11/2025": ("1º Domingo do Advento", "advento"),
-    "07/12/2025": ("2º Domingo do Advento", "advento"),
-    "14/12/2025": ("3º Domingo do Advento", "advento"),
-    "21/12/2025": ("Culto de Natal", "natal"),
-    "28/12/2025": ("1º Domingo após o Natal", "natal")
+   # === Epifania ===
+    "05/01/2025": "Epifania - 1º Domingo após Epifania",
+    "12/01/2025": "Epifania - 2º Domingo após Epifania",
+    "19/01/2025": "Epifania - 3º Domingo após Epifania",
+    "26/01/2025": "Epifania - 4º Domingo após Epifania",
+    "02/02/2025": "Epifania - 5º Domingo após Epifania",
+    "09/02/2025": "Epifania - 6º Domingo após Epifania",
+    "16/02/2025": "Epifania - 7º Domingo após Epifania",
+    "23/02/2025": "Epifania - 8º Domingo após Epifania",
+
+    # === Quaresma ===
+    "02/03/2025": "Quaresma - 1º Domingo da Quaresma",
+    "09/03/2025": "Quaresma - 2º Domingo da Quaresma",
+    "16/03/2025": "Quaresma - 3º Domingo da Quaresma",
+    "23/03/2025": "Quaresma - 4º Domingo da Quaresma",
+    "30/03/2025": "Quaresma - 5º Domingo da Quaresma",
+    "06/04/2025": "Quaresma - Domingo da Paixão",
+    "13/04/2025": "Quaresma - Domingo de Ramos",
+
+    # === Páscoa ===
+    "20/04/2025": "Páscoa - Domingo da Ressurreição",
+    "27/04/2025": "Páscoa - 2º Domingo da Páscoa",
+    "04/05/2025": "Páscoa - 3º Domingo da Páscoa",
+    "11/05/2025": "Páscoa - 4º Domingo da Páscoa",
+    "18/05/2025": "Páscoa - 5º Domingo da Páscoa",
+    "25/05/2025": "Páscoa - 6º Domingo da Páscoa",
+    "01/06/2025": "Páscoa - 7º Domingo da Páscoa",
+
+    # === Pentecostes ===
+    "08/06/2025": "Pentecostes",
+
+    # === Tempo Comum ===
+    "15/06/2025": "Tempo Comum - 2º Domingo após Pentecostes",
+    "22/06/2025": "Tempo Comum - 3º Domingo após Pentecostes",
+    "29/06/2025": "Tempo Comum - 4º Domingo após Pentecostes",
+    "06/07/2025": "Tempo Comum - 5º Domingo após Pentecostes",
+    "13/07/2025": "Tempo Comum - 6º Domingo após Pentecostes",
+    "20/07/2025": "Tempo Comum - 7º Domingo após Pentecostes",
+    "27/07/2025": "Tempo Comum - 8º Domingo após Pentecostes",
+    "03/08/2025": "Tempo Comum - 9º Domingo após Pentecostes",
+    "10/08/2025": "Tempo Comum - 10º Domingo após Pentecostes",
+    "17/08/2025": "Tempo Comum - 11º Domingo após Pentecostes",
+    "24/08/2025": "Tempo Comum - 12º Domingo após Pentecostes",
+    "31/08/2025": "Tempo Comum - 13º Domingo após Pentecostes",
+    "07/09/2025": "Tempo Comum - 14º Domingo após Pentecostes",
+    "14/09/2025": "Tempo Comum - 15º Domingo após Pentecostes",
+    "21/09/2025": "Tempo Comum - 16º Domingo após Pentecostes",
+    "28/09/2025": "Tempo Comum - 17º Domingo após Pentecostes",
+    "05/10/2025": "Tempo Comum - 18º Domingo após Pentecostes",
+    "12/10/2025": "Tempo Comum - 19º Domingo após Pentecostes",
+    "19/10/2025": "Tempo Comum - 20º Domingo após Pentecostes",
+    "26/10/2025": "Tempo Comum - 21º Domingo após Pentecostes",
+    "02/11/2025": "Tempo Comum - 22º Domingo após Pentecostes",
+    "09/11/2025": "Tempo Comum - 23º Domingo após Pentecostes",
+    "16/11/2025": "Tempo Comum - 24º Domingo após Pentecostes",
+    "23/11/2025": "Tempo Comum - 25º Domingo após Pentecostes",
+    "30/11/2025": "Tempo Comum - Domingo de Cristo Rei",
+
+    # === Advento ===
+    "30/11/2025": "Advento - 1º Domingo do Advento",
+    "07/12/2025": "Advento - 2º Domingo do Advento",
+    "14/12/2025": "Advento - 3º Domingo do Advento",
+    "21/12/2025": "Advento - 4º Domingo do Advento",
+
+    # === Natal ===
+    "25/12/2025": "Natal do Senhor",
+    "28/12/2025": "Natal - 1º Domingo após o Natal",
 }
 
 def parse_date_from_card(card):
@@ -114,7 +128,10 @@ def adicionar_sermao():
         print("Data não mapeada no calendário litúrgico 2025.")
         return
 
-    descricao, estacao_slug = calendario[key]
+    descricao_completa = calendario[key]
+    partes = descricao_completa.split(" - ", 1)
+    estacao_slug = slugify(partes[0])
+    descricao = partes[1] if len(partes) > 1 else partes[0]
 
     if not os.path.isfile(HTML_FILE):
         print("index.html não encontrado.")
